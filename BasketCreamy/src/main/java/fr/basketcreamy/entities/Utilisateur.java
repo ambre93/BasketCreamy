f74eea679feae9a63bc99937820ac5835e4c6221
ambre93 <auclerc.virginie@orange.fr>
2022-03-21 12:59:23 +0100

    Utilisateur

--- BasketCreamy/src/main/java/fr/basketcreamy/entities/Utilisateur.java
+++ BasketCreamy/src/main/java/fr/basketcreamy/entities/Utilisateur.java
@@ -1,5 +1,20 @@
 package fr.basketcreamy.entities;
 
 public class Utilisateur {
-
+	private Integer id;
+	private String nom;
+	private String Prenom;
+	private Date dateNaissance;
+	private boolean isActif;
+	private String profil;
+	private String email;
+	private byte[] password;
+	private String telephone;
+	private List<Adresse> adresses;
+	private List<Commande> commandes;
+	private List<CartePaiement> cartesDePaiement;
+	private List<Commentaire> commentaires;
+	private List<ArticlePanier> panier;
+	
+	
 }
