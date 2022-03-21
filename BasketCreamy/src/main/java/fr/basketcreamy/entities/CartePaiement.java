ef85b4c392b151d59119063dee17868f8871b3f2
imenne <imenne_bouaicha@yahoo.fr>
2022-03-21 14:24:57 +0100

    creation entités CartePaiement et Params

---
+++ BasketCreamy/src/main/java/fr/basketcreamy/entities/CartePaiement.java
@@ -0,0 +1,73 @@
+package fr.basketcreamy.entities;
+
+import java.util.Date;
+
+public class CartePaiement {
+	
+	private Integer id;
+	private String nomProprietaire;
+	private String prenomProprietaire;
+	private byte[] numero;
+	private Date dateFinValidite;
+	private byte[] cryptogramme;
+	private Integer utilisateur;
+	public CartePaiement() {
+		
+	}
+	public CartePaiement(String nomProprietaire, String prenomProprietaire, byte[] numero, Date dateFinValidite,
+			byte[] cryptogramme, Integer utilisateur) {
+		super();
+		this.nomProprietaire = nomProprietaire;
+		this.prenomProprietaire = prenomProprietaire;
+		this.numero = numero;
+		this.dateFinValidite = dateFinValidite;
+		this.cryptogramme = cryptogramme;
+		this.utilisateur = utilisateur;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNomProprietaire() {
+		return nomProprietaire;
+	}
+	public void setNomProprietaire(String nomProprietaire) {
+		this.nomProprietaire = nomProprietaire;
+	}
+	public String getPrenomProprietaire() {
+		return prenomProprietaire;
+	}
+	public void setPrenomProprietaire(String prenomProprietaire) {
+		this.prenomProprietaire = prenomProprietaire;
+	}
+	public byte[] getNumero() {
+		return numero;
+	}
+	public void setNumero(byte[] numero) {
+		this.numero = numero;
+	}
+	public Date getDateFinValidite() {
+		return dateFinValidite;
+	}
+	public void setDateFinValidite(Date dateFinValidite) {
+		this.dateFinValidite = dateFinValidite;
+	}
+	public byte[] getCryptogramme() {
+		return cryptogramme;
+	}
+	public void setCryptogramme(byte[] cryptogramme) {
+		this.cryptogramme = cryptogramme;
+	}
+	public Integer getUtilisateur() {
+		return utilisateur;
+	}
+	public void setUtilisateur(Integer utilisateur) {
+		this.utilisateur = utilisateur;
+	}
+	
+	
+	
+
+}
---
+++ BasketCreamy/src/main/java/fr/basketcreamy/entities/Params.java
@@ -0,0 +1,51 @@
+package fr.basketcreamy.entities;
+
+public class Params {
+	private Integer id;
+	private byte[] cle_cryptage_pwd;
+	private byte[] cle_cryptage_cp;
+	
+	
+	public Params() {
+		
+	}
+
+
+	public Params(byte[] cle_cryptage_pwd, byte[] cle_cryptage_cp) {
+		super();
+		this.cle_cryptage_pwd = cle_cryptage_pwd;
+		this.cle_cryptage_cp = cle_cryptage_cp;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public byte[] getCle_cryptage_pwd() {
+		return cle_cryptage_pwd;
+	}
+
+
+	public void setCle_cryptage_pwd(byte[] cle_cryptage_pwd) {
+		this.cle_cryptage_pwd = cle_cryptage_pwd;
+	}
+
+
+	public byte[] getCle_cryptage_cp() {
+		return cle_cryptage_cp;
+	}
+
+
+	public void setCle_cryptage_cp(byte[] cle_cryptage_cp) {
+		this.cle_cryptage_cp = cle_cryptage_cp;
+	}
+
+	
+}
