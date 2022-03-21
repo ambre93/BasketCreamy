cba0e7ea9dab7326de55efaf6f0de4049af4f68f
ambre93 <auclerc.virginie@orange.fr>
2022-03-21 13:19:43 +0100

    Article

--- BasketCreamy/src/main/java/fr/basketcreamy/entities/Article.java
+++ BasketCreamy/src/main/java/fr/basketcreamy/entities/Article.java
@@ -1,5 +1,14 @@
 package fr.basketcreamy.entities;
 
 public class Article {
-
+	private Integer id;
+	private String nom;
+	private String description;
+	private Double prix;
+	private Integer remise;
+	private Integer stock;
+	private Boolean isVendable;
+	private String photos;
+	private String videos;
+	private String commentaires;
 }
