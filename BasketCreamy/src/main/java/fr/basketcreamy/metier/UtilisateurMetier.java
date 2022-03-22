68ddd1a8aaeaa7063ef9c47b4dc15efa2527220f
ambre93 <auclerc.virginie@orange.fr>
2022-03-22 17:24:41 +0100

    Fin de la journée
    Page Web JSF

--- BasketCreamy/src/main/java/fr/basketcreamy/metier/UtilisateurMetier.java
+++ BasketCreamy/src/main/java/fr/basketcreamy/metier/UtilisateurMetier.java
@@ -1,21 +1,28 @@
 package fr.basketcreamy.metier;
 
+import java.security.Key;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import fr.basketcreamy.cryptage.algo.AlgorithmeCryptage;
 import fr.basketcreamy.cryptage.model.connection.MyDataSource;
 import fr.basketcreamy.entities.Utilisateur;
-
+import fr.basketcreamy.enums.EnumCryptage;
 
 public class UtilisateurMetier implements IUtilisateurMetier {
 
+	private IParams params = new Params();
+	
 	@Override
 	public Utilisateur addUtilisateur(Utilisateur utilisateur) throws Exception {
 		
 		Connection connection = MyDataSource.getInstance().getConnection();
-		String requete = "INSERT INTO utilisateur(nom, prenom, dateNaissance, isActif, profil, email, password, telephone, adresses, commandes, cartesDePaiement, commentaires, panier) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		String requete = "INSERT INTO utilisateur(nom, prenom, dateNaissance, isActif, profil, email, password, telephone) VALUES(?, ?, ?, ?, ?, ?, ?)";
 		PreparedStatement ps = connection.prepareStatement(requete, Statement.RETURN_GENERATED_KEYS);
 		ps.setString(1, utilisateur.getNom());
 		ps.setString(2, utilisateur.getPrenom());
@@ -24,12 +31,13 @@ public class UtilisateurMetier implements IUtilisateurMetier {
 		//ps.setLong(5, utilisateur.getProfil());
 		ps.setString(6, utilisateur.getEmail());
 		ps.setString(7, utilisateur.getTelephone());
-		//ps.setString(8, utilisateur.getAdresses());
-		//ps.setBytes(9, utilisateur.getCommandes());
-		//ps.setBytes(10, utilisateur.getCartesDePaiement());
-		//ps.setBytes(11, utilisateur.getCommentaires());
-		//ps.setBytes(12, utilisateur.getPanier());
-		//ps.setBytes(13, utilisateur.getPassword());
+		
+		byte[] cleCryptage = params.getCleCryptagePwd();
+		SecretKey key = new SecretKeySpec(cleCryptage, 0, cleCryptage.length, EnumCryptage.AES.getAlgorithm());
+		byte[] passwordCrypte = AlgorithmeCryptage.encrypt(utilisateur.getPassword().toString(), key, EnumCryptage.AES.getAlgorithm());
+		utilisateur.setPassword(passwordCrypte);
+		
+		ps.setBytes(8, utilisateur.getPassword());
 		
 		ps.executeUpdate();
 		
@@ -45,7 +53,15 @@ public class UtilisateurMetier implements IUtilisateurMetier {
 
 	@Override
 	public Utilisateur seConnecter(String email, String password) throws Exception {
-		// TODO Auto-generated method stub
+		Utilisateur user = Utilisateur.getUserByEmail(email);
+		if (user != null) {
+			byte[] cleCryptage = params.getCleCryptagePwd();
+			Key key = new SecretKeySpec(cleCryptage, EnumCryptage.AES.getAlgorithm());
+			String passwordDataBase = AlgorithmeCryptage.decrypt(user.getPassword(), key, EnumCryptage.AES.getAlgorithm());
+			if (passwordDataBase.equals(password)) {
+				return user;
+			}
+		}
 		return null;
 	}
 
