d9f5061dfa85dad547e997cc9fbdf9b2521ea39e
ambre93 <auclerc.virginie@orange.fr>
2022-03-24 15:42:34 +0100

    GetUserById

--- BasketCreamy/src/main/java/fr/basketcreamy/entities/dao/UserDao.java
+++ BasketCreamy/src/main/java/fr/basketcreamy/entities/dao/UserDao.java
@@ -41,4 +41,62 @@ public class UserDao implements IUserDao {
 		//user.setId(5);
 		return user;
 	}
+
+	@Override
+	public void removeUtilisateur(int userId) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public User getUserByEmail(String email) throws Exception {
+		Connection connection = MyDataSource.getInstance().getConnection();
+		
+		String requete = "SELECT * FROM user WHERE email = ?";
+		PreparedStatement ps = connection.prepareStatement(requete);
+		ps.setString(1, email);
+		
+		ResultSet rs = ps.executeQuery();
+		User user = null;
+		if (rs.next()) {
+			user = new User();
+			user.setId(rs.getInt("id"));
+			user.setNom(rs.getString("nom"));
+			user.setPrenom(rs.getString("prenom"));
+			user.setEmail(rs.getString("email"));
+			user.setPassword(rs.getBytes("password"));
+			user.setProfil(rs.getString("profil"));
+			user.setTelephone(rs.getString("telephone"));
+		}
+		if (connection != null && !connection.isClosed()) {
+			connection.close();
+		}
+		return user;
+	}
+	
+	@Override
+	public User getUserById(Integer userId) throws Exception {
+		Connection connection = MyDataSource.getInstance().getConnection();
+		
+		String requete = "SELECT * FROM user WHERE utilisateur_id = ?";
+		PreparedStatement ps = connection.prepareStatement(requete);
+		ps.setInt(1, userId);
+		
+		ResultSet rs = ps.executeQuery();
+		User user = null;
+		if (rs.next()) {
+			user = new User();
+			user.setId(rs.getInt("id"));
+			user.setNom(rs.getString("nom"));
+			user.setPrenom(rs.getString("prenom"));
+			user.setEmail(rs.getString("email"));
+			user.setPassword(rs.getBytes("password"));
+			user.setProfil(rs.getString("profil"));
+			user.setTelephone(rs.getString("telephone"));
+		}
+		if (connection != null && !connection.isClosed()) {
+			connection.close();
+		}
+		return user;
+	}
 }
